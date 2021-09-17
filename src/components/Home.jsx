e17f7e03e390af92d1579b85f89ff8a2865a7374
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-16 23:57:04 -0400

    :memo: Examples with hook useState
    Create a example using useState twice

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react'
+
 const Home = () => {
-    return (
-        <div className="home">
-            <h2>Homepage</h2>
-        </div>
-    )
+
+    /** Hook useState */
+    const [name, setName] = useState(`Who is my poshito`)
+    const [icon, setIcon] = useState(`❓`)
+
+        const handleButton = () => {
+        setName('My poshito is Nathalie')
+        setIcon(`🐔`)
+        }
+
+        return (
+            <div className="home">
+                <h2>Homepage</h2>
+                <p>{ name } { icon }</p>
+                <button onClick={handleButton}>Let me see! 👀</button>
+            </div>
+        )
 }
 
 export default Home
