9dcb10709d1436bff8a29d30b1a87aa64f8f875f
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-16 23:12:27 -0400

    :bulb: Example code with handled events
    Managin to events with onClick, and example with anonymous arrow functions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,24 @@
 const Home = () => {
+
+    const handledButton = (event) => {
+        console.log('Hello my friend!', event.target);
+    }
+
+    /** Function to handled Event of button */
+    /* const handledAnotherButton = (name, event) => {
+        console.log(`Hello ${name} ${event.tar}`);
+    } */
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handledButton}>Click Me</button>
+
+            { /**Example of event with anonymous arrow function*/
+            
+            /* <button onClick={ (event) => {
+                handledAnotherButton(`Yulianny`, event);
+            }}>Anothe Click</button> */}
         </div>
     )
 }
