0d877a3719fdfc506144d2ca2be15bce6cb136ba
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-17 16:28:25 -0400

    :memo: useState and map for list blog
    Create a hook useState for the list blog, and map the list in the component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
+import { useState } from 'react'
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'First post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
+        {title: 'Second post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
+        {title: 'Thirty post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
+    ])
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                    <h2>{ blog.title }</h2>
+                    <p>Escrito por { blog.author }</p>
+                </div>
+            ))}    
         </div>
     )
 }
