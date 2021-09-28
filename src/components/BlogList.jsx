283acf9c0d54952e5b8090226fcb3d48349f6239
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-28 14:01:34 -0400

    :construction: Create handleDelete post

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,12 +1,13 @@
-const BlogList = ({ blogs }) => {
+const BlogList = ({ blogs, title, handleDelete }) => {
     
     return (
         <div className="blog-list">
-            
+            <h1>{ title }</h1>
             {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
+                <div className="blog-preview" key={ blog.id }>
                     <h2>{ blog.title }</h2>
                     <p>Escrito por { blog.author }</p>
+                    <button onClick={() => handleDelete(blog.id)}>Eliminar blog</button>
                 </div>
             ))}    
         </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,15 +2,20 @@ import { useState } from 'react'
 import BlogList from './BlogList'
 
 const Home = () => {
+    /** HOOK USE STATE */
     const [blogs, setBlogs] = useState([
-        {title: 'First post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
-        {title: 'Second post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
-        {title: 'Thirty post', body: 'Lorem ipsum' , author: 'Yulianny B', id: 1},
-    ])
+        { title: 'First post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1 },
+        { title: 'Second post', body: 'Lorem ipsum' , author: 'Yulianny', id: 2 },
+        { title: 'Thirty post', body: 'Lorem ipsum' , author: 'Yulianny B', id: 3 },
+    ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return (
         <div className="home">
-            <BlogList blogs={blogs}/>
+            <BlogList blogs={blogs} title="All Post" handleDelete={handleDelete} />
         </div>
     )
 }
