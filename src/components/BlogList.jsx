b9ae2b0b3a1bf0976adb85e6e55b68e8f5853737
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-30 23:40:53 -0400

    :memo: Create useEffect
    Explain useEffect and dependencies for rendering one time the DOM

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,17 +1,15 @@
 const BlogList = ({ blogs, title, handleDelete }) => {
-    
-    return (
-        <div className="blog-list">
-            <h1>{ title }</h1>
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={ blog.id }>
-                    <h2>{ blog.title }</h2>
-                    <p>Escrito por { blog.author }</p>
-                    <button onClick={() => handleDelete(blog.id)}>Eliminar blog</button>
-                </div>
-            ))}    
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Escrito por {blog.author}</p>
+          <button onClick={() => handleDelete(blog.id)}>Eliminar blog</button>
         </div>
-    )
-}
-
-export default BlogList
+      ))}
+    </div>
+  );
+};
+export default BlogList;
