95a9987dfb7e2adb5691b0d90c27077438ab8462
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-01 01:36:48 -0400

    :memo: fetching data with useEffect
    Delete handleDelete and fetching data with useEffect

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h1>{title}</h1>
@@ -7,20 +6,6 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Escrito por {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Eliminar blog</button>
-=======
-const BlogList = ({ blogs }) => {
-    
-    return (
-        <div className="blog-list">
-            
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>Escrito por { blog.author }</p>
-                </div>
-            ))}    
->>>>>>> parent of 283acf9 (:construction: Create handleDelete post)
         </div>
       ))}
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,46 +2,23 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-<<<<<<< HEAD
   /** HOOKS USE STATE */
-  const [blogs, setBlogs] = useState([
-    { title: "First post", body: "Lorem ipsum", author: "Yulianny", id: 1 },
-    { title: "Second post", body: "Lorem ipsum", author: "Yulianny", id: 2 },
-    { title: "Thirty post", body: "Lorem ipsum", author: "Yulianny B", id: 3 },
-  ]);
-
-  const [name, setName] = useState(" 🐣 ");
-=======
-    const [blogs, setBlogs] = useState([
-        {title: 'First post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
-        {title: 'Second post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1},
-        {title: 'Thirty post', body: 'Lorem ipsum' , author: 'Yulianny B', id: 1},
-    ])
-
-    return (
-        <div className="home">
-            <BlogList blogs={blogs}/>
-        </div>
-    )
-}
->>>>>>> parent of 283acf9 (:construction: Create handleDelete post)
+  const [blogs, setBlogs] = useState(null);
 
   useEffect(() => {
-    console.log(name);
-  }, [name]);
-
-  /** Delete post */
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+    /** Fetch request */
+    fetch("http://localhost:8080/blogs")
+      .then((response) => {
+        return response.json();
+      })
+      .then((database) => {
+        setBlogs(database);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Post" handleDelete={handleDelete} />
-      <button onClick={() => setName(" 🐥 pioooo :3")}>
-        Empolla el poshito
-      </button>
+      {blogs && <BlogList blogs={blogs} title="All Post" />}
     </div>
   );
 };
