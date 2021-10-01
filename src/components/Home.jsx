6bf10b05556fe88a37b814cfa1c4b825640bd155
Yulianny Betancourt <flipsdulce@gmail.com>
2021-09-30 23:55:06 -0400

    :memo: Create useEffect
    Add useEffect and dependencies for change the state on useStade, and rendering one time the DOM

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,34 @@
-import { useState } from 'react'
-import BlogList from './BlogList'
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    /** HOOK USE STATE */
-    const [blogs, setBlogs] = useState([
-        { title: 'First post', body: 'Lorem ipsum' , author: 'Yulianny', id: 1 },
-        { title: 'Second post', body: 'Lorem ipsum' , author: 'Yulianny', id: 2 },
-        { title: 'Thirty post', body: 'Lorem ipsum' , author: 'Yulianny B', id: 3 },
-    ]);
+  /** HOOKS USE STATE */
+  const [blogs, setBlogs] = useState([
+    { title: "First post", body: "Lorem ipsum", author: "Yulianny", id: 1 },
+    { title: "Second post", body: "Lorem ipsum", author: "Yulianny", id: 2 },
+    { title: "Thirty post", body: "Lorem ipsum", author: "Yulianny B", id: 3 },
+  ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-    return (
-        <div className="home">
-            <BlogList blogs={blogs} title="All Post" handleDelete={handleDelete} />
-        </div>
-    )
-}
+  const [name, setName] = useState(" 🐣 ");
 
-export default Home
+  useEffect(() => {
+    console.log(name);
+  }, [name]);
+
+  /** Delete post */
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Post" handleDelete={handleDelete} />
+      <button onClick={() => setName(" 🐥 pioooo :3")}>
+        Empolla el poshito
+      </button>
+    </div>
+  );
+};
+
+export default Home;
