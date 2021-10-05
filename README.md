41917ec0973c80a5894c62749d8c1a702046a3bf
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-05 18:19:16 -0400

    :sparkles: Use the react router

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Simple Blog with React
 
+## Tree of components
 
+![Tree components](https://i.imgur.com/7E7ewV2.png "Tree Component")
--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import Home from './components/Home';
-import Navbar from './components/Navbar';
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
-const App = () =>{
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-      <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
-  )
-}
+    </Router>
+  );
+};
 
 export default App;
