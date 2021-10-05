1b1ce8447146856b52d20ac9485deeaf906a3731
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-05 15:15:41 -0400

    :memo: Add Handling Fetch Errors
    Create branch Add Handling Fetch Errors to my documentation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,33 @@ import BlogList from "./BlogList";
 const Home = () => {
   /** HOOKS USE STATE */
   const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     /** Fetch request */
     fetch("http://localhost:8080/blogs")
       .then((response) => {
-        return response.json();
+        if (response.ok) {
+          return response.json();
+        }
+        throw Error("Could not fetch the data for that resource");
       })
       .then((data) => {
         setBlogs(data);
+      })
+      .catch((error) => {
+        setError(error.message);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {loading && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Post" />}
     </div>
   );
