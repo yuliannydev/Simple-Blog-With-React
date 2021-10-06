cd50c820e2d896f46244e1586d489bdc3b10829f
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-06 15:54:59 -0400

    :sparkles: Submit new blog post done

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,23 +1,70 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, settitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Yulianny Betancourt");
+  const [submit, setSubmit] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const blog = { title, body, author };
+
+    setSubmit(true);
+
+    fetch("http://localhost:8080/blogs/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      setSubmit(false);
+    });
+  };
   return (
     <div className="create">
       <h2>Add new Blog!</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <label htmlFor="">
           Title
-          <input type="text" required />
+          <input
+            type="text"
+            placeholder="Add the title"
+            value={title}
+            required
+            onChange={(event) => settitle(event.target.value)}
+          />
         </label>
 
         {/** Body */}
         <label htmlFor="">
           Body
-          <textarea name="" id="" cols="30" rows="10" required></textarea>
+          <textarea
+            name=""
+            placeholder="Add the content"
+            value={body}
+            id=""
+            cols="30"
+            rows="10"
+            required
+            onChange={(event) => setBody(event.target.value)}
+          ></textarea>
         </label>
-        <select name="" id="">
-          <option value="">Yulianny Betancourt</option>
-          <option value="">Constribuidor(a)</option>
+
+        {/** select author  */}
+        <select
+          name=""
+          id=""
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
+          <option value="Yulianny Betancourt">Yulianny Betancourt</option>
+          <option value="Constribuidor(a)">Constribuidor(a)</option>
         </select>
-        <button type="submit">Enviar</button>
+
+        {/** Subtmit the blog post */}
+        {!submit && <button>Send blog</button>}
+        {submit && <button disabled>Charging...</button>}
       </form>
     </div>
   );
