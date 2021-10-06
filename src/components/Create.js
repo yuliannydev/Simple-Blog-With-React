1a5e9f7c4a6be3a17b19baa7f7713ab888fc2589
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-06 12:41:03 -0400

    :sparkles: Create controlled Inputs (form)

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,24 @@
 const Create = () => {
   return (
     <div className="create">
-      <h1>Add new Blog!</h1>
+      <h2>Add new Blog!</h2>
+      <form action="">
+        <label htmlFor="">
+          Title
+          <input type="text" required />
+        </label>
+
+        {/** Body */}
+        <label htmlFor="">
+          Body
+          <textarea name="" id="" cols="30" rows="10" required></textarea>
+        </label>
+        <select name="" id="">
+          <option value="">Yulianny Betancourt</option>
+          <option value="">Constribuidor(a)</option>
+        </select>
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 };
