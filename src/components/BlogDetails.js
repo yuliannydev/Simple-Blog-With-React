556abf4d50026e0d0caf1dccddf78a501ea4a24f
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-06 11:48:53 -0400

    :sparkles: Construct the Render from blog details

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,10 +1,25 @@
 import { useParams } from "react-router";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const { data, loading, error } = useFetch(
+    `http://localhost:8080/blogs/${id}`
+  );
+
   return (
     <div className="blog-details">
-      <h2>Hola desde blog details {id} </h2>
+      {loading && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {data && (
+        <article>
+          <h2> {data.title} </h2>
+          <p>Escrito por: {data.author}</p>
+          <div>
+            <p> {data.body} </p>
+          </div>
+        </article>
+      )}
     </div>
   );
 };
