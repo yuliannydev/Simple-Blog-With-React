84b0dcd822f1ab0c6abd2e1068e809a73d0afcd6
Yulianny Betancourt <flipsdulce@gmail.com>
2021-10-06 09:25:42 -0400

    :sparkles: Crete Route Parameters with useParams

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,10 +1,14 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h1>{title}</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
+          <Link to={`blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+          </Link>
           <p>Escrito por {blog.author}</p>
         </div>
       ))}
